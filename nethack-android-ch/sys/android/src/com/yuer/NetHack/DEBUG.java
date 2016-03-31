ab4b771cd462db5ab01c044819e0ed555be8bd85
SunnyEric <956768385@qq.com>
2016-03-31 15:03:35 +0800

    更改软件包名，与英文版分开

---
+++ nethack-android-ch/sys/android/src/com/yuer/NetHack/DEBUG.java
@@ -0,0 +1,7 @@
+package com.yuer.NetHack;
+
+public class DEBUG
+{
+	static boolean isOn() { return false; }
+	static boolean runTrace() { return false; }
+}
---
+++ nethack-android-ch/sys/android/src/com/yuer/NetHack/KeyAction.java
@@ -0,0 +1,11 @@
+package com.yuer.NetHack;
+
+public class KeyAction
+{
+	public static final int SystemDefault = 0;
+	public static final int ZoomIn = -1;
+	public static final int ZoomOut = -2;
+	public static final int Keyboard = -3;
+	public static final int Control = -4;
+	public static final int Meta = -5;
+}
